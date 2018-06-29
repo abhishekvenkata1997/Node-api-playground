e8b0b542bc86ca3b6d4c3d1055521fa85a5abdca
abhishek venkata <abhishekvenkata.199715@gmail.com>
2018-06-30 03:16:53 +0530

    Updating for deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect( 'mongodb://Abhishek Venkata:abhi0454@ds123171.mlab.com:23171/todoapp');
+  // || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 
 app.use(bodyParser.json());
@@ -49,8 +50,8 @@ app.get('/todos/:id',(req,res)=>{
 
 
 
-app.listen(3000,()=>{
-  console.log("Server starts running on localhost:3000");
+app.listen(port,()=>{
+  console.log(`Server starts running on ${port}`);
 })
 
 module.exports = {
