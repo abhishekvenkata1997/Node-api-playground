8bbf22e6c4572c9e0d2cacf0e3531ca6fa4a6cdd
abhishek venkata <abhishekvenkata.199715@gmail.com>
2018-06-30 01:44:28 +0530

    Updating Server.js to have specific todo id route installed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 
@@ -30,8 +31,24 @@ app.get('/todos',(req,res)=>{
   });
 });
 
+//GET /todos/:id To get the specific todo
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+      return res.status(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+      if(!todo){
+        return res.status(404).send();
+      }
+      res.send({todo});
+    }).catch((e)=> {
+      res.status(400).send();
+    });
+});
 
-//GET /todos/i... To get the specific todo
+
+
 app.listen(3000,()=>{
   console.log("Server starts running on localhost:3000");
 })
